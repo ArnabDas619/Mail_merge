16af3ed3475c525ef966e1ddf1abbbced643f24b
Arnab Das <arnabdas.619@gmail.com>
2024-01-06 20:07:20 +0530

    Added instruction in readme file and added Placeholder in main.py

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Mail_merge
 Creating text messages for people using data from a text file.
+
+To added your list of names.
+1. go to input folder 
+2. go to Names folder
+3. open up invited_names.txt
+4. add your list of names in the format mentioned
+
+To add custom test message. 
+1. go to input folder 
+2. go to letters folder 
+3. open up starting_letter.txt 
+4. replace the message according to your liking
+5. keep in mind to use "[name]" where you want to add the custom name.
+
--- main.py
+++ main.py
@@ -1,17 +1,15 @@
+PLACEHOLDER = "[name]"
 # read the list of invited people
 with open("Input/Names/invited_names.txt") as file:
     str_names = file.read()
     Invited_list = str_names.splitlines()
-    print(Invited_list)
+
 
 # read the starting letter
 with open("Input/Letters/starting_letter.txt") as file:
     str_letter = file.read()
 
-
 for name in Invited_list:
-    new_letter = str_letter.replace("[name]", str(name))
-    with open(f"Output/ReadyToSend/{name}.txt", "w") as file :
+    new_letter = str_letter.replace(PLACEHOLDER, str(name))
+    with open(f"Output/ReadyToSend/letter_for_{name}.txt", "w") as file:
         file.write(new_letter)
-
-
